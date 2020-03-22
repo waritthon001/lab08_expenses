36af3d24270ef430600fa5f6a3524c817e905870
waritthonten <waritthon_auttama@cmu.ac.th>
2020-03-23 01:20:40 +0700

    last one

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
     <div>
       <div className="container mt-5">
         <div className="card">
-          <img src={process.env.PUBLIC_URL+'/andreas-klassen.jpg'} className='card-img-top' alt="..." />
+          <img src={'https://firebasestorage.googleapis.com/v0/b/lab08-expenses-e4a44.appspot.com/o/IMG_2499.jpg?alt=media&token=bf78edd3-694e-482b-addf-0e14ae806d65'} className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
-            <p>Dome Potikanond (610610999)</p>
+            <p>Waritthon Auttama(610610612)</p>
             <p>This app use Google Firebase as backend.</p>
             <Link to="/">
               <h3>
